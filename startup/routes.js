13535afad84600d066f4101ad37f1dd279423fb5
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-01 14:41:12 +0530

    feat: add route paths in the routing setup

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,18 @@
+const bus = require('../routes/bus')
+const booking = require('../routes/booking')
+const seat = require('../routes/seat')
 const route = require('../routes/route')
 const user =  require('../routes/users')
 const express = require('express')
+const auth = require('../middleware/auth')
 
 module.exports = function (app){
     app.use(express.json())
     app.use(express.urlencoded)
     app.use('/api/v1/user/signup',user)
+    app.use('/api/v1/user/login',auth)
     app.use('/api/v1/route',route)
+    app.use('/api.v1/seat', seat)
+    app.use('/api/v1/booking', booking)
+    app.use('/api/v1/bus',bus)
 }
