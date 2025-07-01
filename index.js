7f49e042e74496fb1eae9c03916609bbd064f1bc
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-01 23:21:37 +0530

    fix: remove commented-out route in index.js

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const app = express()
 require('dotenv').config()
 
 db
-
-// app.get('/', (req, res) => {
-//     res.send('Welcome to TripTix API')  })
-
 require('./startup/routes')(app)
-
 
 app.listen(3000, () => {
     console.log(`Listening on port 3000...`)
