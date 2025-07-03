2da7becd7c98e0e0f1cad257bd0034edebc0241a
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-03 13:52:53 +0530

    fix: correct typo in seat route path and urlencoded()

--- startup/routes.js
+++ startup/routes.js
@@ -7,12 +7,12 @@ const auth = require('../routes/auth')
 const express = require('express')
 
 module.exports = function(app){
-    app.use(express.json())
-    // app.use(express.urlencoded)
+    app.use(express.json()) 
+    app.use(express.urlencoded())
     app.use('/api/v1/user',user)
     app.use('/api/v1/login',auth)
     app.use('/api/v1/route',route)
-    app.use('/api.v1/seat', seat)
+    app.use('/api/v1/seat', seat)
     app.use('/api/v1/booking', booking)
     app.use('/api/v1/bus',bus)
 }
