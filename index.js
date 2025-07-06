a0353255e3ab1cb6ccec5453b8ce5fe12f9e8fb0
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-06 13:33:11 +0530

    feat: add cors

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const cors = require('cors')
 const db = require('./config/db')
 const express = require('express')
 const app = express()
 require('dotenv').config()
+
+app.use(cors({
+    origin:'*',
+    methods: ['GET','POST','PUT','PATCH'],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders:["Authorization"],
+    credentials: true
+}))
 
 db
 require('./startup/routes')(app)
