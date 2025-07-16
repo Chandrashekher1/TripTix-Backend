5149710ee05a0371519015c87f52bc68dbe85716
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-16 19:20:45 +0530

    feat: add socket.io routes

---
+++ routes/socket.js
@@ -0,0 +1,68 @@
+const { Seat } = require('../models/seat')
+const { Types } = require('mongoose')
+
+const handleSocketConnection = (io) => {
+  io.on('connection', (socket) => {
+    socket.on('lockSeats', async ({ busId, seatIds, userId }) => {
+
+      if (!Types.ObjectId.isValid(userId)) {
+        console.error('Invalid userId:', userId)
+        return
+      }
+      await Seat.updateMany(
+        { _id: { $in: seatIds },
+            busId 
+        },
+        {
+          $set: {
+            status: 'locked',
+            userId: new Types.ObjectId(userId),
+            lockedUntil: new Date(Date.now() + 5 * 60 * 1000)
+          }
+        }
+      )
+      io.emit('seatsUpdated', { seatIds, status: 'locked', userId })
+
+      setTimeout(async () => {
+        await Seat.updateMany(
+          { _id: { $in: seatIds },
+            status: 'locked',
+            userId: new Types.ObjectId(userId),
+            lockedUntil: { $lte: new Date() }  
+        },
+          {
+            $set: {
+              status: 'available',
+              userId: null,
+              lockedUntil: null
+            }
+          }
+
+        )
+        io.emit('seatsUpdated', { seatIds, status: 'available' });
+      },1*60*1000)
+
+    });
+
+    socket.on('unlockSeats', async ({ seatIds }) => {
+      await Seat.updateMany(
+        { _id: { $in: seatIds } },
+        {
+          $set: {
+            status: 'available',
+            userId: null,
+            lockedUntil: null
+          }
+        }
+      )
+
+      io.emit('seatsUpdated', { seatIds, status: 'available' })
+    });
+
+    socket.on('disconnect', () => {
+      console.log('User disconnected: ' + socket.id)
+    })
+  })
+}
+
+module.exports = handleSocketConnection
