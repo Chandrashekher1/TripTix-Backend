534690acbf9bac732ff8e98e371162e8bc385048
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-16 19:12:37 +0530

    fix: remove unused fields

--- models/booking.js
+++ models/booking.js
@@ -1,57 +1,51 @@
-const { required } = require('joi')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const BookingSchema = mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "Users"
-    },
-    busId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "Buses"
-    },
-    seatId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "Seat"
-    },
-    passengerDetails: [
-        {
-            name:{
-                type:String,
-                required: true,
-                lowercase:true
-            },
-            age:{
-                type: String,
-                required: true,
-                lowercase:true
-            },
-            gender: {
-                type: String,
-                required: true,
-                lowercase:true
-            },
-            seatPreference: {
-                type: String,
-                required: true,
-                lowercase: true
-            }
-        },
-       
-    ],
-     status: {
-        type: String,
-        lowercase:true
-    },
-    createAt:{
-        type: Date,
-        default: Date.now()
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Users"
+  },
+  busId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Buses"
+  },
+  seatIds: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Seat",
+      required: true
     }
-})
+  ],
+  passengerDetails: [
+    {
+      name: {
+        type: String,
+        required: true,
+        lowercase: true
+      },
+      age: {
+        type: String,
+        required: true,
+        lowercase: true
+      },
+      gender: {
+        type: String,
+        required: true,
+        lowercase: true
+      }
+    }
+  ],
+  status: {
+    type: String,
+    default: "confirmed"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-const BookingModel = mongoose.model('Booking',BookingSchema)
-
-module.exports.Booking = BookingModel
+const BookingModel = mongoose.model('Booking', BookingSchema);
+module.exports.Booking = BookingModel;
