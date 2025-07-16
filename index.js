757af1aa2d6361f997552a1864a7da808afa1237
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-16 19:11:50 +0530

    feat: add socket configuration

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
+const socket = require('./routes/socket')
+const http = require('http')
 const cors = require('cors')
 const db = require('./config/db')
 const express = require('express')
 const app = express()
 require('dotenv').config()
 
+const server =  http.createServer(app) // http server
+const {Server} = require('socket.io') // socket server
+
+const io = new Server(server, {
+    cors : {
+        origin:'*',
+        methods: ['GET','POST','PUT','PATCH'],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        exposedHeaders:["Authorization"],
+        credentials: true
+    }
+})
+
+global.io // for access in routes
+
+socket(io)
+
 app.use(cors({
-    origin:'*',
-    methods: ['GET','POST','PUT','PATCH'],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders:["Authorization"],
-    credentials: true
-}))
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Authorization'],
+  credentials: true
+}));
 
 db
 require('./startup/routes')(app)
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log(`Listening on port 3000...`)
 })
 
